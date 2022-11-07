680f93eb3e0f34a7a5eeabed4c8492c1747a8c64
Robert Emerencia <robert.emerencia@businessdecision.be>
2022-11-07 22:27:49 +0100

    Add backward/forward dependency calculation

--- tableau-workbook-extractor.py
+++ tableau-workbook-extractor.py
@@ -1,25 +1,11 @@
-from contextlib import contextmanager
-import sys, os
+from twefunctions import *
 import easygui
 from tableaudocumentapi import Workbook
 import pandas as pd
-import numpy as np
-
-# suppress console output when workbook is opened
-# source: http://thesmithfam.org/blog/2012/10/25/temporarily-suppress-console-output-in-python/
-@contextmanager
-def suppress_stdout():
-    with open(os.devnull, "w") as devnull:
-        old_stdout = sys.stdout
-        sys.stdout = devnull
-        try:  
-            yield
-        finally:
-            sys.stdout = old_stdout
 
 # prompt user for twb file and extract file/directory names
 print("Prompt for input Tableau workbook...")
-inpFile = easygui.fileopenbox(filetypes=['*.twb'])
+inpFile = easygui.fileopenbox(filetypes = ['*.twb'])
 inpFileDirectory = os.path.dirname(inpFile)
 inpFileName = os.path.splitext(os.path.basename(inpFile))[0]
 outFile = inpFileDirectory + '\\' + inpFileName + '.csv'
@@ -29,10 +15,12 @@ print("Extract data sources and fields from workbook...")
 with suppress_stdout():
     inpTwb = Workbook(inpFile)
 df1 = pd.DataFrame(inpTwb.datasources, columns = ["data_source"])
-df1["fields"] = df1.apply(lambda x: list(x.data_source.fields.values()), axis = 1)
+df1["fields"] = df1.apply(lambda x: \
+    list(x.data_source.fields.values()), axis = 1)
 dsAttr = ["name", "caption"]
 for attr in dsAttr:
-    df1["data_source_" + attr] = df1.apply(lambda x: getattr(x.data_source, attr), axis = 1)
+    df1["data_source_" + attr] = df1.apply(lambda x: \
+         getattr(x.data_source, attr), axis = 1)
 df2 = df1.apply(lambda x: pd.Series(x['fields']), axis = 1)
 dfList = [df1, df2]
 df = pd.concat(dfList, axis = 1)
@@ -44,12 +32,61 @@ df = pd.melt(df, id_vars = colId, value_vars = colVal)
 df = df[df["value"].notnull()]
 
 # extract field attributes
-fieldAttr = ["id", "caption", "datatype", "role", "type", "alias", "calculation", "description", "hidden"]
+fieldAttr = ["id", "caption", "datatype", "role", "type", "alias", \
+    "calculation", "description", "hidden"]
 for attr in fieldAttr:
     df["field_" + attr] = df.apply(lambda x: getattr(x.value, attr) , axis = 1)
 
 # additional transformations
 df["field_hidden"] = np.where(df["field_hidden"] == "true", 1, 0)
+df[["data_source_caption", "field_caption", "field_calculation"]] = \
+    df[["data_source_caption", "field_caption",
+     "field_calculation"]].fillna('')
+
+# create data sources & fields mapping dictionary
+dictMapping = fieldCalculationMappingTable(df, "data_source_name", \
+    "data_source_caption", "field_id", "field_caption")
+
+# create cleaned copy of field calculations
+# replace na by empty string otherwise errors due to NaN
+df["field_calculation_corr"] = df.apply(lambda x: \
+    fieldCalculationClean(x.field_calculation, x.data_source_name), axis = 1)
+
+# apply field mappings to calculations
+df["field_calculation_corr"] = df["field_calculation_corr"].apply(lambda x: \
+    fieldCalculationMapping(dictMapping, x))
+
+# merge field and data source name fields
+df["field_label"] = np.where(df["field_caption"] == '', \
+    df["field_id"], '[' + df["field_caption"] + ']')
+df["source_label"] = "[" + np.where(df["data_source_caption"] == '', 
+                                   df["data_source_name"], \
+                                    df["data_source_caption"]) + "]"
+
+# filter out duplicate parameter rows
+lstParam = list(df[df["source_label"] == "[Parameters]"]["field_label"])
+df["field_is_param_duplicate"] = df.apply(lambda x: \
+    isParamDuplicate(lstParam, x.source_label, x.field_label), axis = 1)
+df = df[df["field_is_param_duplicate"] == 0]
+
+# create list of unique combinations of data sources + field names
+df["source_field_label"] = df["source_label"] + "." + df["field_label"]
+lstFields = list(dict.fromkeys(list(df["source_field_label"])))
+
+# get list of field dependencies
+df["field_calculation_dependencies"] = \
+    df["field_calculation_corr"].apply(lambda x: \
+        fieldCalculationDependencies(lstFields, x))
+
+# calculate type of field
+df["field_category"] = df.apply(lambda x: \
+    fieldCategory(x.field_label, x.field_calculation_dependencies), axis = 1)
+
+# expand dependencies to full lists of backward and forward dependencies
+df["field_backward_dependencies"] = \
+    df["source_field_label"].apply(lambda x: getBackwardDependencies(df, x))
+df["field_forward_dependencies"] = \
+    df["source_field_label"].apply(lambda x: getForwardDependencies(df, x))
 
 # remove intermediate results
 colRemove = ["data_source", "fields", "variable", "value"]
---
+++ twefunctions.py
@@ -0,0 +1,102 @@
+import sys, os
+from contextlib import contextmanager
+import numpy as np
+import re
+
+# suppress console output when workbook is opened
+# source: http://thesmithfam.org/blog/2012/10/25/temporarily-suppress-console-output-in-python/
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+# dictionary of [source ID].[field ID] -> [source caption].[label caption]
+def fieldCalculationMappingTable(df, colFromSource, \
+    colToSource, colFromField, colToField):
+    dfRes = df.copy()
+    # original [source].[field]
+    dfRes["from"] = "[" + dfRes[colFromSource] + "]." + dfRes[colFromField]
+    # mapped [source].[field]
+    dfRes["to"] = ("[" + np.where(dfRes[colToSource] == '', \
+        dfRes[colFromSource], dfRes[colToSource]) 
+                   + "]." + np.where(dfRes[colToField] == '', \
+                    dfRes[colFromField], "[" + dfRes[colToField] + "]"))
+    dfRes = dfRes[["from", "to"]]
+    arrRes = np.array(dfRes)
+    # dict conversion automatically deduplicates mappings
+    dictRes = dict(arrRes[1:]) 
+    return dictRes
+
+# clean up an expression x
+def fieldCalculationClean(x, s):
+    # remove all comments (important for dependencies)
+    # pattern: starts with 2 forward slashes and ends with new line \n
+    res = re.sub(r"\/{2}.*\n", '', x)
+    # replace internal field reference (not . + [field]) by [source].[field]
+    res = re.sub(r"([^.])(\[.*\])", r"\1" + "[" + s + r"].\2", res)
+    return res
+
+# apply a set of replacements to a string
+# note: the mapping (field_id -> field_caption) appears to be unique
+# (renaming in Tableau is reverted automatically when reopening the workbook)
+def fieldCalculationMapping(d, x):
+    res = x
+    for key in d: res = res.replace(key, d.get(key))
+    return res
+
+# list direct dependencies in a calculation x based on a list l
+def fieldCalculationDependencies(l, x):
+    return [s for s in l if s in x]
+
+# check if a field is a parameter duplicate
+def isParamDuplicate(p, s, x):
+    return x in p and s != "[Parameters]"
+
+# get field category (parameter, field or calculated field)
+def fieldCategory(s, d):
+    if s.startswith("[Parameters]."): return "Parameter"
+    if len(d) == 0: return "Field"
+    else: return "Calculated Field"
+
+# recursively get all backward dependencies of a field
+def getBackwardDependencies(df, f, level = 0):
+    x = df.loc[df.source_field_label == f]
+    depList = list(x.field_calculation_dependencies)
+    cat = list(x.field_category)[0]
+    lst = []
+    
+    # add dependency
+    if level > 0: lst += [{"dependency": f, \
+        "level": "-{0}".format(level), "category": cat}]
+    
+    # add dependencies of dependency
+    if len(depList) > 0:
+        # remove reference by copying the list
+        for y in depList[0].copy():
+            lst += getBackwardDependencies(df, y, level + 1)
+    return lst
+
+# recursively get all forward dependencies of a field
+def getForwardDependencies(df, f, level = 0):
+    x = df.loc[df.source_field_label == f]
+    depList = df.apply(lambda z: \
+        f in z.field_calculation_dependencies, axis = 1)
+    depList  = list(df.loc[depList]["source_field_label"])
+    cat = list(x["field_category"])[0]
+    lst = []
+    
+    # add dependency
+    if level > 0: lst += [{"dependency": f, \
+        "level": "+{0}".format(level), "category": cat}]
+    
+    # add dependencies of dependency
+    if len(depList) > 0:
+        # remove reference by copying the list
+        for y in depList.copy(): 
+            lst += getForwardDependencies(df, y, level + 1)
+    return lst
