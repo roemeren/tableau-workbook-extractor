c59e20a1cb1405f8f3c45985e9979d96932d2f1c
Robert Emerencia <robert.emerencia@gmail.com>
2025-10-17 23:04:22 +0200

    Modify Dash app to automatically identify sample files & store output files separately

--- app/shared/utils.py
+++ app/shared/utils.py
@@ -4,8 +4,11 @@ from pathlib import Path
 
 STATIC_FOLDER = os.path.join('web', 'static')
 UPLOAD_FOLDER = os.path.join(STATIC_FOLDER, 'uploads')
-# relative path used in download links (public URL)
-UPLOAD_URL_PATH = os.path.relpath(UPLOAD_FOLDER, start='web')
+OUTPUT_FOLDER = os.path.join(STATIC_FOLDER, 'output')
+OUTPUT_FOLDER_URL = os.path.relpath(OUTPUT_FOLDER, start="web")
+
+# Keep SAMPLE_FOLDER as a Path object for easy file listing with glob() in app
+SAMPLE_FOLDER = Path(STATIC_FOLDER) / 'sample'
 COLOR_PROCESSING = '#343a40'
 VERSION_FILE = Path("VERSION")
 REPO_URL = "https://github.com/roemeren/tableau-workbook-extractor"
